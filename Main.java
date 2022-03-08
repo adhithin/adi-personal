1c54b10bba03d98ab1338833f529e27e191e0430
adhithinarayanamurthy <adhithi.nmurthy07@gmail.com>
2022-03-08 10:19:37 -0800

    fixed IntByReference

--- Main.java
+++ Main.java
@@ -1,5 +1,21 @@
 public class Main {
+
+    public Option () {
+
+    }
+
+
+    public int val (int n ) {
+        System.out.print(n);
+    }
+
     public static void main(String[] args) {
+
+        for (int i = 0; i < 6; i++){
+            Option op = new Option ();
+            op.val(i);
+        }
+
         System.out.print("hello world");
     }
 }
